f535a0587f0d333273c3a382adb674985ce14d53
John Desmond <johndesmond631@gmail.com>
2023-11-16 23:56:28 -0500

    first attempt at text to speech

--- main.py
+++ main.py
@@ -5,10 +5,18 @@
 
 
 # Save the chat history to send back and forth for context 
+import os
+
+import openai
+from openai import OpenAI
+from dotenv import load_dotenv
+from fastapi import FastAPI, UploadFile
 
 
+load_dotenv()
 
-from fastapi import FastAPI
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.organization = os.getenv("OPEN_AI_ORG")
 
 app = FastAPI()
 
@@ -16,3 +24,18 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/talk")
+async def post_audio(file: UploadFile):
+    # convert audio to text
+    
+    client = OpenAI()
+    # UploadFile by default uploads to file.filename
+    audio_file = open(file.filename, "rb")
+    # client.audio.transcriptions.create
+    transcript = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file
+    )
+    print(transcript)
+
--- notes.md
+++ notes.md
@@ -19,3 +19,30 @@ pip install openai
 
 platform.openai.com  
 manage account  
+
+got to here before failing to get credits:  
+https://youtu.be/4y1a4syMJHM?t=432
+
+
+```
+pip install python-dotenv
+pip install python-multipart
+```
+
+https://platform.openai.com/docs/libraries/libraries  
+https://platform.openai.com/docs/guides/speech-to-text  
+
+Use postman.  
+```
+POST
+localhost:8000/talk
+form data: file
+KEY: file
+
+```
+
+Stop at:
+```
+    raise self._make_status_error_from_response(err.response) from None
+openai.RateLimitError: Error code: 429 - {'error': {'message': 'You exceeded your current quota, please check your plan and billing details.', 'type': 'insufficient_quota', 'param': None, 'code': 'insufficient_quota'}}
+```
